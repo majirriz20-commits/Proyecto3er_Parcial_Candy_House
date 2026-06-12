0303b030aca95fa9a480934289f43fb9ea21354b
81678pau <pgr81678@lasallebajio.edu.mx>
2026-06-12 16:25:52 -0600

    conectar mainactivity al navgraph

--- app/src/main/java/com/example/candyhouse/MainActivity.kt
+++ app/src/main/java/com/example/candyhouse/MainActivity.kt
@@ -4,15 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.candyhouse.Screen.InicioScreen
+import androidx.navigation.compose.rememberNavController
+import com.example.candyhouse.Screen.NavGraph
 import com.example.candyhouse.ui.theme.CandyHouseTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,25 +14,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CandyHouseTheme {
-                        InicioScreen()
-                    }
-                }
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CandyHouseTheme {
-        Greeting("Android")
     }
 }
