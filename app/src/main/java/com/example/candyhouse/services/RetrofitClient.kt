08395ac8e91e31f28b2fd3ef97357f4ec15b42eb
MAM1534 <dianaandrade.g1566@gmail.com>
2026-06-15 16:22:43 -0600

    feat: unificar navegacion y conectar pantalla compras al NavGraph

--- app/src/main/java/com/example/candyhouse/services/RetrofitClient.kt
+++ app/src/main/java/com/example/candyhouse/services/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "http://172.20.14.155:3000/"
 
     val apiService: CandyApiService by lazy {
         Retrofit.Builder()
